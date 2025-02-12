82de7b4213bc083c1bc6c8b6952d336c337eab7e
Pallo Bangura <herespallo@gmail.com>
2025-02-12 12:38:14 +0000

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Vite configuration
 export default defineConfig({
+  root: 'src',  // Ensures that Vite looks for the `index.html` and other files inside `src`
   plugins: [react()],
+  build: {
+    outDir: '../dist',  // This ensures the build is output to the root directory
+  },
 });
